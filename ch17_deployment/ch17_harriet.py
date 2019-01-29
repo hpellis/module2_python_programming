e8719bb1c8dadafe7681a0fd588ea706887b0660
hpellis <42347904+hpellis@users.noreply.github.com>
2019-01-29 10:33:09 +0100

    initial commit

---
+++ ch17_deployment/ch17_harriet.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 29 09:25:22 2019
+
+@author: 612383249
+"""
+
+#----------------------------------------------------------------------------------------------------------------------
+# CHAPTER 17 - DEPLOYING CODE
+#----------------------------------------------------------------------------------------------------------------------
+
+#this chapter covers deploying code to a web server
+#heroku is a cloud-based platform 
+#install the heroku command line interface and configure
+#login to heroku via the command line
+#navigate to the folder holding application
+#enter command 'heroku create'
+#this is assigned a random URL
+#push your code to the git repositiory created by heroku
+#'git push heroku master'
+#check how many apps are running by typing 'heroku ps:scale web=1'
+#to open the app, you can use the command 'heroku open'
+#you can add other people to the heroku repo by clicking on the app and then using the access tab to add collaborators
